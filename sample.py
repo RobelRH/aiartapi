7629d90d908a11dffb5866f99744190a066f3c28
RobelRH <robel.redda28@gmail.com>
2022-10-29 00:50:23 +0300

    first commit

---
+++ sample.py
@@ -0,0 +1,24 @@
+from cProfile import run
+from operator import mod
+import replicate
+import webbrowser
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        print("i got something")
+        request_data = request.data
+        request_data = json.loads(request_data.decode('utf-8'))
+        prompt = request_data['prompt']
+        model = replicate.models.get("stability-ai/stable-diffusion")
+        output_url = model.predict(prompt=prompt)[0]
+        return {"generated": output_url}
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
