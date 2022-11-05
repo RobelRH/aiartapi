124fdbecceb8836b90dea7e45922f2de9535147b
RobelRH <robel.redda28@gmail.com>
2022-11-05 23:31:19 +0300

    post test 5

--- app.py
+++ app.py
@@ -14,15 +14,14 @@ def home():
     if request.method == 'POST':
         # print("i got something")
         request_data = request.data
-        return {"abc" : "request_data"}
-        # request_data = json.loads(request_data.decode('utf-8'))
-        # prompt = request_data['prompt']
-        # model = replicate.models.get("stability-ai/stable-diffusion")
-        # output_url = model.predict(prompt=prompt)[0]
-        # return {"generated": output_url}
+        request_data = json.loads(request_data.decode('utf-8'))
+        prompt = request_data['prompt']
+        model = replicate.models.get("stability-ai/stable-diffusion")
+        output_url = model.predict(prompt=prompt)[0]
+        return {"generated": output_url}
         # return "post api detected"
 
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000, debug=True)
-    app.run()
+    app.run(debug=True)
